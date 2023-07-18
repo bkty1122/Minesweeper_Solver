65466bbd6dda7bd2c083efeac912bacb4c75f073
bkty1122 <kwoktszyi1122@gmail.com>
2023-07-19 00:02:44 +0800

    new file:   assets/.DS_Store 	new file:   connect_azure.py

---
+++ connect_azure.py
@@ -0,0 +1,3 @@
+#Connect to Azure SQL Database, returning data and executing queries
+#https://learn.microsoft.com/en-us/azure/azure-sql/database/connect-query-python?view=azuresql-mi 
+import pyodbc
